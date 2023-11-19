d452bdabda65e7bba63bf349e1a0023487c2e899
Rita-Centeno <ritafalcaocenteno@gmail.com>
2023-11-19 13:34:35 +0000

    mudança no main

---
+++ Capstone/main.py
@@ -0,0 +1,54 @@
+import streamlit as st
+
+from streamlit_option_menu import option_menu
+
+
+import home, account, schedule, about
+st.set_page_config(
+        page_title="Doc-IT-Right",
+)
+
+
+
+class MultiApp:
+
+    def __init__(self):
+        self.apps = []
+
+    def add_app(self, title, func):
+
+        self.apps.append({
+            "title": title,
+            "function": func
+        })
+
+    def run():
+        # app = st.sidebar(
+        with st.sidebar:        
+            app = option_menu(
+                menu_title='Doc-IT-Right ',
+                options=['Home','Account','Schedules','about'],
+                icons=['house-fill','person-circle','chat-fill','info-circle-fill'],
+                menu_icon='chat-text-fill',
+                default_index=1,
+                styles={
+                    "container": {"padding": "5!important","background-color":'black'},
+        "icon": {"color": "white", "font-size": "23px"}, 
+        "nav-link": {"color":"white","font-size": "20px", "text-align": "left", "margin":"0px", "--hover-color": "blue"},
+        "nav-link-selected": {"background-color": "#02ab21"},}
+                
+                )
+
+        
+        if app == "Home":
+            home.app()
+        if app == "Account":
+            account.app()          
+        if app == 'Schedules':
+            schedule.app()
+        if app == 'about':
+            about.app()    
+             
+          
+             
+    run()            
