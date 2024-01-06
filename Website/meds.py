5960fb49233f2ed22e8248d6f2a092eca58a0c98
Bruna Faria <brunafdfaria@gmail.com>
2024-01-06 12:31:57 +0000

    requirements 2

--- Website/meds.py
+++ Website/meds.py
@@ -2,7 +2,7 @@ import PyPDF2
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
 
-pdf_data = PdfReader("C:\\Users\\ritaf\\Documents\\ESTUDOS\\NOVA IMS\\3º Ano\\Capstone Project\\Projeto\\meds.pdf")
+pdf_data = PdfReader("C:\\Users\\bruna\\Documents\\Bruna\\school\\IMS\\3ºano\\1ºsemestre\\CP_CapstoneProject\\meds.pdf")
 pdf_text = ""
 
 for i, page in enumerate(pdf_data.pages):
